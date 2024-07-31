a011c625692e19f81791dd5cec8b2c1b0c0ac169
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-07-31 16:35:13 +0700

    fix(home) : fix double header

--- feapp/src/App.js
+++ feapp/src/App.js
@@ -1,24 +1,24 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
-//import Contact from './pages/Contact';
+// import Contact from './pages/Contact';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Header />
-        <div className="content p-4">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            {/* <Route path="/contact" component={Contact} /> */}
-          </Switch>
-        </div>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            {/* <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+        </main>
       </div>
     </Router>
   );
--- feapp/src/pages/Home.jsx
+++ feapp/src/pages/Home.jsx
@@ -2,13 +2,11 @@
 
 import React from 'react';
 import InputForm from '../components/InputForm';
-import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <main className="flex-grow flex justify-center items-center p-4">
         <div className="w-full sm:w-2/3 lg:w-1/2">
           <InputForm />
