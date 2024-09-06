2e87afb7a82a133f9153660d66cc91a682dd6149
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-09-06 13:05:12 +0700

    Fix : App

--- feapp/src/App.js
+++ feapp/src/App.js
@@ -3,17 +3,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Footer';
+import Footer from './components/Footer'
+import ResultTest from './components/ResultTest.jsx'
+import InputTest from './components/InputTest.jsx'
 import Home from './pages/Home';
 import About from './pages/About';
 import Result from './pages/Result';
 import Detail from './pages/Detail';
-import './index.css'; // Import Tailwind CSS
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div className="App">
         <Header />
         <main className="flex-grow">
           <Routes>
@@ -21,6 +22,8 @@ const App = () => {
             <Route path="/about" element={<About />} />
             <Route path="/result" element={<Result />} />
             <Route path="/detail" element={<Detail />} />
+            <Route path="/resulttest" element={<ResultTest />} />
+            <Route path="/inputtest" element={<InputTest />} />
           </Routes>
         </main>
         <Footer />
--- feapp/src/pages/Home.jsx
+++ feapp/src/pages/Home.jsx
@@ -3,17 +3,24 @@
 import React from 'react';
 import InputForm from '../components/InputForm';
 
-
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex-grow flex justify-center items-center p-4">
+      <div className="flex-grow flex flex-col justify-center items-center p-4">
         <div className="w-full sm:w-2/3 lg:w-1/2">
+          <div className="h-48 sm:h-64 overflow-hidden mb-4"> {/* Adjust height for mobile */}
+            <img
+              src="https://via.placeholder.com/400x200"
+              alt="Placeholder"
+              className="w-full h-full object-cover"
+            />
+          </div>
           <InputForm />
         </div>
-      </main>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
