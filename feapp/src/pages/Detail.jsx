2e2daa9bab278f75700a187b709bf914c6049ae1
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-09-20 22:40:37 +0700

    Update detail

--- feapp/src/pages/Detail.jsx
+++ feapp/src/pages/Detail.jsx
@@ -1,12 +1,25 @@
+// src/pages/Detail.jsx
+
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Papa from 'papaparse'; // Import PapaParse
 import csvFile from '../data/yogyakarta_budaya.csv'; // Adjust the path to your CSV file
 
 const Detail = () => {
-  const { id } = useParams(); // Get ID from URL if using React Router
+  const { slug } = useParams(); // Get slug from URL
   const [place, setPlace] = useState(null);
   const [placesData, setPlacesData] = useState([]); // State to store the parsed CSV data
+
+  const slugify = (text) => {
+    return text
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, '-')        // Ganti spasi dengan tanda "-"
+      .replace(/[^\w\-]+/g, '')    // Hapus karakter non-word
+      .replace(/\-\-+/g, '-')      // Ganti banyak tanda "-" dengan satu "-"
+      .replace(/^-+/, '')          // Hapus tanda "-" di awal
+      .replace(/-+$/, '');         // Hapus tanda "-" di akhir
+  };
 
   useEffect(() => {
     // Parse CSV file using PapaParse
@@ -25,11 +38,11 @@ const Detail = () => {
 
   useEffect(() => {
     if (placesData.length > 0) {
-      // Find the place by ID after CSV is loaded
-      const selectedPlace = placesData.find(place => place.Place_Id === id);
+      // Find the place by slug after CSV is loaded
+      const selectedPlace = placesData.find(place => slugify(place.Place_Name) === slug);
       setPlace(selectedPlace);
     }
-  }, [id, placesData]);
+  }, [slug, placesData]);
 
   if (!place) {
     return <div>Loading...</div>;
@@ -39,7 +52,7 @@ const Detail = () => {
     <div className="p-4">
       <div className="mb-4">
         <img
-          src="https://via.placeholder.com/400x200?text=Image+Not+Available" // Keeping image placeholder
+          src="https://via.placeholder.com/400x200?text=Image+Not+Available" // Placeholder image
           alt={place.Place_Name}
           className="w-full h-auto"
         />
