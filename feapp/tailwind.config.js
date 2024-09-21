9431a436cc9a6cdb6a04a3d0524a98de7c50cf32
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-09-21 22:40:25 +0700

    Update tailwind

--- feapp/tailwind.config.js
+++ feapp/tailwind.config.js
@@ -1,12 +1,11 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Atur path yang sesuai dengan file React Anda
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
 }
+
