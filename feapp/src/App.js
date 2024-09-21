0e82d5737043c100c0e109edf918fa6985a82e2e
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-09-21 23:14:52 +0700

    Update js and css pages

--- feapp/src/App.js
+++ feapp/src/App.js
@@ -3,27 +3,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Footer'
-import ResultTest from './components/ResultTest.jsx'
+import Footer from './components/Footer';
+import ResultTest from './components/ResultTest.jsx';
 import Home from './pages/Home';
-import AllPlaces from './pages/AllPlaces.jsx'
+import AllPlaces from './pages/AllPlaces.jsx';
 import About from './pages/About';
-import Result from './pages/Result';
-import ResulTestPages from './pages/ResultTestPages.jsx'
-import Detail from './pages/Detail'; // Halaman Detail
-
+import ResulTestPages from './pages/ResultTestPages.jsx';
+import Detail from './pages/Detail';
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
+      <div className="flex flex-col h-screen">
         <Header />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/allplaces" element={<AllPlaces />} />
             <Route path="/about" element={<About />} />
-            <Route path="/result" element={<Result />} />
             <Route path="/resulttest" element={<ResultTest />} />
             <Route path="/resultpages" element={<ResulTestPages />} />
             <Route path="/detail/:slug" element={<Detail />} /> {/* Perbarui route menjadi slug */}
