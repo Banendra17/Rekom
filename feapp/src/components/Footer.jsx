33c25388f1bcb3b4016652ed4acc4cc3130b4a9e
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-09-21 23:06:05 +0700

    Update footer

--- feapp/src/components/Footer.jsx
+++ feapp/src/components/Footer.jsx
@@ -4,9 +4,11 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 mt-8">
+    <footer className="bg-white shadow-t p-4 mt-8">
       <div className="container mx-auto text-center">
-        <p className="text-sm sm:text-base">© 2024 My Website. All rights reserved.</p>
+        <p className="text-gray-600 text-sm sm:text-base">
+          © 2024 My Website. All rights reserved.
+        </p>
       </div>
     </footer>
   );
