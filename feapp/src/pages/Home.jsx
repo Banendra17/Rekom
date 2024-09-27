c7fb961bff422b54bb1a927603a78379459a712a
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-09-27 22:57:51 +0700

    Update home

--- feapp/src/pages/Home.jsx
+++ feapp/src/pages/Home.jsx
@@ -5,21 +5,17 @@ import InputForm from '../components/InputForm';
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
-      <div className="flex-grow flex flex-col justify-center items-center px-4">
-        <div className="w-full sm:w-2/3 lg:w-1/3">
-          {/* Gambar */}
-          <div className="h-48 sm:h-56 overflow-hidden rounded-lg mb-6">
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow flex flex-col justify-center items-center p-4">
+        <div className="w-full sm:w-2/3 lg:w-1/2">
+          <div className="h-48 sm:h-64 overflow-hidden mb-4"> {/* Adjust height for mobile */}
             <img
               src="https://via.placeholder.com/400x200"
               alt="Placeholder"
-              className="w-full h-full object-cover shadow-md"
+              className="w-full h-full object-cover"
             />
           </div>
-          {/* Form Input */}
-          <div className="p-4 bg-white rounded-lg shadow-md">
-            <InputForm />
-          </div>
+          <InputForm />
         </div>
       </div>
     </div>
@@ -27,3 +23,4 @@ const Home = () => {
 };
 
 export default Home;
+
