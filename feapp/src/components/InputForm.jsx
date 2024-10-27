891b51a37f6a3093adb49f0fcbb18d504385c6ce
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-10-27 14:16:16 +0700

    Update button

--- feapp/src/components/InputForm.jsx
+++ feapp/src/components/InputForm.jsx
@@ -11,12 +11,9 @@ const InputForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      // Kirim request ke Flask API
       const response = await axios.post('http://localhost:5000/recommend', {
         description: inputValue,
       });
-
-      // Arahkan ke halaman hasil dan kirim data rekomendasi melalui state
       navigate('/result', { state: { recommendations: response.data } });
     } catch (error) {
       console.error('Error fetching recommendations:', error);
@@ -35,7 +32,7 @@ const InputForm = () => {
       />
       <button
         type="submit"
-        className="px-6 py-3 bg-blue-500 text-white text-lg rounded-lg hover:bg-blue-600 transition-colors"
+        className="w-full px-6 py-3 bg-blue-500 text-white text-lg rounded-lg hover:bg-blue-600 transition-colors"
       >
         Submit
       </button>
